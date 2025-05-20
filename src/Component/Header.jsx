e6232a3b3e8e298f175f6926436f1fd9f34bb7ad
Prakash-b1 <pinnub23@gmail.com>
2025-05-20 17:56:57 +0530

    updated

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header className="bg-white shadow-md py-4 px-6 flex items-center  justify-between">
-      <div className="flex items-center mx-auto md:mx-0 max-w-7xl space-x-2">
+    <header className="bg-white shadow-md w-full py-4 px-6 justify-center flex items-center ">
+      <div className="flex items-center mx-auto md:mx-0 max-w-7xl w-full space-x-2">
         <span className="text-5xl  font-extrabold ">ab media</span>
       </div>
     </header>
